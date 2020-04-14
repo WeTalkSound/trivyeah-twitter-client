a6c886f6cfd24421ba95f05b485d9f99566579af
Etin <etinosaobaseki@gmail.com>
2020-04-14 18:57:35 +0100

    Changes

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,28 @@
 const functions = require('firebase-functions')
 const firebaseAdmin = require('firebase-admin')
 const config = require('./config.js')
-var Twit = require('twit')
- 
+const Twit = require('twit')
+const TrivYeah = require('../services/trivyeah')
+const FireRepo = require('../services/firebase-repo')
+
 var T = new Twit(config)
+// var trivyeah = new TrivYeah({ tenant_key:"app" })
+
+firebaseAdmin.initializeApp(functions.config().firebase);
+// var gameRepository = new FireRepo(firebaseAdmin.database().ref('games'))
+var gameRepository = (firebaseAdmin.database().ref('games'))
+
+exports.duplicateSNG = functions.https.onRequest((request, response) => {
+    let first = {a:3}
+    gameRepository.on("value", async function (snapshot) {
+        first = console.log(snapshot.val())
+    })
+    T.get('statuses/mentions_timeline')
+    console.log(first)
+    response.send(first)
+})
+
+exports.startNewGame = functions.pubsub.schedule('every 1 minute').onRun((context) => {
+    gameRepository.first()
+    T.get('statuses/mentions_timeline')
+})
